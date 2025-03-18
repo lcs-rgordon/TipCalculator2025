c74ca1205ebe66326a2f46a473232996e6ad4fef
Russell Gordon <rgordon@lcs.on.ca>
2025-03-18 13:36:43 -0400

    Initial Commit

---
+++ TipCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipCalculator
+//
+//  Created by Russell Gordon on 2025-03-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
